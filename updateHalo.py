9a5ac2768b3b59d934eabed4fdbfb3e7c3ffa45d
sugarbeet77 <38092701+sugarbeet77@users.noreply.github.com>
2022-04-10 20:17:58 +0800

    Update updateHalo.py

--- updateHalo.py
+++ updateHalo.py
@@ -26,7 +26,7 @@ def getVersion():
         exit()
 
     if oldVersion != newVersion:
-        os.system('./updateHaloLinux.sh '+newVersion)
+        os.system('cd /root/autoUpdateHalo/ && ./updateHaloLinux.sh '+newVersion)
     else:
         print('已是最新版本')
 
